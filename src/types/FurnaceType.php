32ffdc7bbf9c15d91b2b7fad157aa20a659612da
MrRedTv <redtv.dev@gmail.com>
2026-07-30 18:27:58 +0200

    Create FurnaceType.php

---
+++ src/types/FurnaceType.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+namespace pocketmine\network\mcpe\protocol\types\furnace;
+
+final class FurnaceType {
+    public const NONE = 0;
+    public const FURNACE = 1;
+    public const BLAST_FURNACE = 2;
+    public const SMOKER = 3;
+}
